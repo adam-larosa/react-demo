02577ca5482ac1fd986e6d856e71b7e940b56431
Adam La Rosa <adam.larosa@flatironschool.com>
2023-06-14 09:01:29 -0700

    fin

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 
+import { useState } from 'react'
+
 
 function App() {
+
+	const [ number, setNumber ] = useState( 0 )
+
+	const increment = () => setNumber( number + 1 )
+
 	return (
 		<div>
-			<h1>hello meow!</h1>
+			<Child theNumber={ number }/>
+			<button onClick={ increment }>change number</button>
 		</div>
 	);
 }
+
+
+const Child = ({ theNumber }) => {
+
+	return (
+		<div>
+			<h1>{ theNumber }</h1>
+		</div>
+	)
+}
+
+
 
 export default App;
